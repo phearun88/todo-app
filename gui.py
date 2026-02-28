6212c18ac84123cf78a45411c89648b1e6ca5784
phearun168 <phin.phearun2013@gmail.com>
2026-02-28 14:23:18 -0600

    Implement the complete button

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+from unittest import case
+
 import function
 
 import FreeSimpleGUI as sg
@@ -10,9 +12,13 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_todos(), key="todos",
                       enable_events=True, size=(45,10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label],[input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],[input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica',20))
 while True:
     event, values = window.read()
@@ -35,6 +41,15 @@ while True:
             todos[index] = new_todo
             function.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = function.get_todos()
+            todos.remove(todo_to_complete)
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             print("edit")
             window['todo'].update(value=values['todos'][0])
